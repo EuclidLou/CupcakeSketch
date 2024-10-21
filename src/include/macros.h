f9ea314fcfc61e3285c8804f911c653b4681c3ca
EuclidLou <1562857339@qq.com>
2024-10-21 20:24:23 +0800

    Add non-update minhash elimination

--- src/include/macros.h
+++ src/include/macros.h
@@ -7,14 +7,15 @@
 #define USE_TOWER       0
 #define USE_CS          1
 #define METRICS         1
-#define MEMORY_1_24     (int)(1024)
-#define K               320000
+#define MEMORY_1_24     (int)(16*1024*1024)
+#define K               80000
 #define DATASET         "caida"
-#define ITEM_SIZE       21
-#define REPEAT          20
+#define ITEM_SIZE       15
+#define REPEAT          1
 #define S_FACTOR        0.5
 #define HEAVY_BIAS      1
 #define SWAP_FACTOR     3
+#define TIMES_RESAMPLE  1
 
 
 
@@ -27,7 +28,8 @@ TODO:   1. 数据集切分->OK
         2. 使用Count Sketch作为小流计数->OK
         3. 调参脚本->OK
         4. 小流数据回读
-        5. CAIDA的数据格式？
+        5. CAIDA的数据格式->OK
         6. 改进测试函数的数据集入口，节约数据集初始化时间->OK
-        7. 多层CS
+        7. 多层CS->OK
+        8. 多个集合
 */
--- src/include/sketch_minhash.h
+++ src/include/sketch_minhash.h
@@ -125,8 +125,11 @@ public:
         // {
         //     min_hash_value_1[i] = std::min(min_hash_value_1[i], HASH::hash(HASH::hash(item, hash_seed[i]), freq));
         // }
-        int min_idx =  static_cast<int>(HASH::hash(HASH::hash(item, index_s), freq) % static_cast<uint64_t>(HASH_CNT));
-        min_hash_value_1[min_idx] = std::min(min_hash_value_1[min_idx], HASH::hash(HASH::hash(item, hash_seed[min_idx]), freq));
+        for (int i=0; i<TIMES_RESAMPLE; i++)
+        {
+            int min_idx =  static_cast<int>(HASH::hash(HASH::hash(item, index_s+i), freq) % static_cast<uint64_t>(HASH_CNT));
+            min_hash_value_1[min_idx] = std::min(min_hash_value_1[min_idx], HASH::hash(HASH::hash(item, hash_seed[min_idx]), freq));
+        }
     }
 
     void insert2(data_t item)
@@ -140,8 +143,11 @@ public:
         // {
         //     min_hash_value_2[i] = std::min(min_hash_value_2[i], HASH::hash(HASH::hash(item, hash_seed[i]), freq));
         // }
-        int min_idx =  static_cast<int>(HASH::hash(HASH::hash(item, index_s), freq) % static_cast<uint64_t>(HASH_CNT));
-        min_hash_value_2[min_idx] = std::min(min_hash_value_2[min_idx], HASH::hash(HASH::hash(item, hash_seed[min_idx]), freq));
+        for (int i=0; i<TIMES_RESAMPLE; i++)
+        {
+            int min_idx =  static_cast<int>(HASH::hash(HASH::hash(item, index_s+i), freq) % static_cast<uint64_t>(HASH_CNT));
+            min_hash_value_2[min_idx] = std::min(min_hash_value_2[min_idx], HASH::hash(HASH::hash(item, hash_seed[min_idx]), freq));
+        }
     }
 
     double similarity()
@@ -149,11 +155,14 @@ public:
         // LOG_DEBUG("into similarity_minhash()");
         double similarity = 0;
         int same_hash = 0, all_hash = HASH_CNT;
-        for (int i = 0; i < all_hash; i++)
-            if (min_hash_value_1[i] == min_hash_value_2[i])
-                same_hash++;
+        for (int i = 0; i < HASH_CNT; i++){
+            if (min_hash_value_1[i] == min_hash_value_2[i]){
+                if (min_hash_value_1[i] != 0xffffffffffffffff) same_hash++;
+                else all_hash--;
+            }
+        }
         similarity = 1.0 * same_hash / all_hash;
-        // LOG_DEBUG("same_hash: %d, all_hash: %d", same_hash, all_hash);
+        LOG_DEBUG("same_hash: %d, all_hash: %d", same_hash, all_hash);
         // LOG_RESULT("similarity: %lf", similarity);
         // LOG_DEBUG("exit similarity_minhash()");
         return similarity;
